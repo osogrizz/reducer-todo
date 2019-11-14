5d87bb5ecc4b62e7564549dedb7dc6d3be3447e8
osogrizz <lhtor@gmail.com>
2019-11-13 19:04:02 -0800

    Adds switch statement to reducer

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,11 +1,27 @@
-import React, {  usewReducer } from 'react'
+import React, {  useReducer } from 'react'
+
 
 export const initialState = {
-item: 'Lern about reducers',
-completed: false,
-  id: 3892987589
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589,
 }
 
 export const reducer = (state, action) => {
-
+  switch(action.type) {
+    case 'item':
+      return {
+        item: this.state.item,
+    }
+    case 'completed':
+      return {
+        completed: !this.state.completed
+    }  
+    case 'id': 
+      return {
+        id: this.state.id
+    }
+    default: 
+     return initialState
+  }
 }
