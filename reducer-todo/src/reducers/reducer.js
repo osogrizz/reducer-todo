13a86bcdb0c2f0b3a3538cfcae4d1a8449554af9
osogrizz <lhtor@gmail.com>
2019-11-13 18:36:01 -0800

    init commit, adds reducer

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,11 @@
+import React, {  usewReducer } from 'react'
+
+export const initialState = {
+item: 'Lern about reducers',
+completed: false,
+  id: 3892987589
+}
+
+export const reducer = (state, action) => {
+
+}
